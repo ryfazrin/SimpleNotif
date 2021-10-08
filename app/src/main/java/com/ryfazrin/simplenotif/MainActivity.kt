62db5c551668e8ce3a0df444faf7005cb9b2e88e
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-10-08 10:17:52 +0800

    Update MainActivity.kt
    Menambahkan method aksi sendNotification

--- app/src/main/java/com/ryfazrin/simplenotif/MainActivity.kt
+++ app/src/main/java/com/ryfazrin/simplenotif/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.ryfazrin.simplenotif
 
+import android.app.NotificationManager
+import android.content.Context
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.app.NotificationCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    // aksi untuk onClick pada button
+    fun sendNotification(view: View) {
+        val mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val mBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_baseline_notifications_24)
+            .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.ic_baseline_notifications_24))
+            .setContentTitle(resources.getString(R.string.content_title))
+            .setContentText(resources.getString(R.string.content_text))
+            .setSubText(resources.getString(R.string.subtext))
+            .setAutoCancel(true)
+
+        val notification = mBuilder.build()
+
+        mNotificationManager.notify(NOTIFICATION_ID, notification)
+    }
+
+    companion object {
+        private const val NOTIFICATION_ID = 1
+        private const val CHANNEL_ID = "channel_01"
+        private const val CHANNEL_NAME = "dicoding channel"
+    }
+
 }
